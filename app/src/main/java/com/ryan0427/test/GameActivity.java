b2f99546997d12743424e85ac92bfd3155e5af4f
0427ryan <70810066+0427ryan@users.noreply.github.com>
2022-05-16 16:26:01 +0800

    finish refresh

--- app/src/main/java/com/ryan0427/test/GameActivity.java
+++ app/src/main/java/com/ryan0427/test/GameActivity.java
@@ -3,6 +3,7 @@ package com.ryan0427.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class GameActivity extends AppCompatActivity {
             {0, 0, 0, 0},
             {0, 0, 0, 0}
     };
+
+    private TextView[] views;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +33,56 @@ public class GameActivity extends AppCompatActivity {
                 {0, 0, 0, 0},
                 {0, 0, 0, 0}
         };
+        views = new TextView[]{
+                findViewById(R.id.cell00),findViewById(R.id.cell01),findViewById(R.id.cell02),findViewById(R.id.cell03),
+                findViewById(R.id.cell10),findViewById(R.id.cell11),findViewById(R.id.cell12),findViewById(R.id.cell13),
+                findViewById(R.id.cell20),findViewById(R.id.cell21),findViewById(R.id.cell22),findViewById(R.id.cell23),
+                findViewById(R.id.cell30),findViewById(R.id.cell31),findViewById(R.id.cell32),findViewById(R.id.cell33)
+        };
+        refresh();
     }
 
-    public void refresh(View view){
-
+    public void refresh(){
+        for(int i = 0 ; i < 16 ; i++){
+            int value = arr[i / 4][i % 4];
+            views[i].setText(value > 0 ? String.valueOf(value) : "");
+        }
     }
 
     public void toLeft(View view){
-
-        refresh(view);
+        for(int i = 0 ; i < 4 ; i++){
+            for(int j = 0 ; j < 4 ; j++){
+                if(arr[i][j] != 0){
+                    continue;
+                }
+                int temp = 4;
+                for(int k = j + 1 ; k < 4 ; k++){
+                    if(arr[i][k] != 0){
+                        temp = k;
+                        break;
+                    }
+                }
+                if(temp == 4)
+                    continue;
+                arr[i][j] = arr[i][temp];
+                arr[i][temp] = 0;
+                if(j != 0 && arr[i][j - 1] == arr[i][j]){
+                    arr[i][j - 1] *= 2;
+                }
+            }
+        }
+        refresh();
     }
     public void toRight(View view){
 
-        refresh(view);
+        refresh();
     }
     public void toTop(View view){
 
-        refresh(view);
+        refresh();
     }
     public void toBottom(View view){
 
-        refresh(view);
+        refresh();
     }
 }
